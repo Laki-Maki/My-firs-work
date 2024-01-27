7482256d8b9c409b9685bb69154e558e30444b81
George <admin@itproger.com>
2024-01-27 15:49:59 +0300

    last

--- New_Project/New_Project.cpp
+++ New_Project/New_Project.cpp
@@ -1,12 +1,18 @@
 ﻿#include <iostream>
 using namespace std;
 
+void pr(int x, int y) {
+    cout << "x * y = " << x * y << endl;
+}
+
 int main()
 {
     int a = 45;
     int b = 10;
+    
     cout << a << endl;
     cout << b << endl;
+    pr(a, b);
 
     return 0;
 }
